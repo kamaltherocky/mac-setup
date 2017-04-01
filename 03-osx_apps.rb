ef7cd922fe428522fbbba17847d1c11998ba1bb0
Kamal <kmuralidharan@paypal.com>
2017-03-31 23:08:25 -0700

    added chrome, docker and atom

--- 03-osx_apps.rb
+++ 03-osx_apps.rb
@@ -1,6 +1,7 @@
 # OS-X Applications
 # Install all the apps using HomeBrew Cask
 
+# iTerm - Terminal Emulator - https://www.iterm2.com
 dep "iTerm2" do
 	met? {
 		shell? "brew cask list iterm2"
@@ -11,7 +12,62 @@ dep "iTerm2" do
 	}
 end
 
+# Google Chrome - https://www.google.com/chrome/
+dep "Google Chrome" do
+        met? {
+                shell? "brew cask list google-chrome"
+        }
+
+        meet {
+                shell("brew cask install google-chrome")
+        }
+end
+
+# Atom - Text Editor from Github Team - https://atom.io
+dep "Atom" do
+        met? {
+                shell? "brew cask list atom"
+        }
+
+        meet {
+                shell("brew cask install atom")
+        }
+end
+
+# Docker Toolbox - Docker Machine, Engine, Kitematic & Compose
+# https://www.docker.com/products/docker-toolbox
+dep "Docker Toolbox" do
+        met? {
+                shell? "brew cask list docker-toolbox"
+        }
+
+        meet {
+                shell("brew cask install docker-toolbox")
+        }
+end
+
+# Docker for mac - https://www.docker.com/docker-mac
+dep "Docker for Mac" do
+        met? {
+                shell? "brew cask list docker"
+        }
+
+        meet {
+                shell("brew cask install docker")
+        }
+end
 
 dep "osx-apps-all" do
+	# --------------- Terminal / CLI ------------------------#
 	requires "iTerm2"
+
+	# --------------- Text Editors ------------------------#
+	requires "Atom"
+
+	# --------------- Browsers ------------------------#
+	requires "Google Chrome"
+
+	# --------------- Docker Ecosystem ------------------------#
+	requires "Docker Toolbox"
+	requires "Docker for Mac"
 end
